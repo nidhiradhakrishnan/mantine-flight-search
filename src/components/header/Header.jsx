739b1bd86901c11238c4554bcbe96b09615c623f
Sreenidhi S R <sreenidhisr07@gmail.com>
2024-08-26 12:34:57 +0530

    feat: "header completed"

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  NavLink,
+  Button,
+  Image,
+  Container,
+  Flex,
+} from "@mantine/core";
+import { useHover } from '@mantine/hooks';
+import headerImage from "../Images/headerImage.png"; 
+import useStyles from "./style";
+
+const Header = () => {
+  const { classes } = useStyles();
+  const { hovered, ref } = useHover();
+
+  const navlinks = [
+    { key: 1, name: "Home", link: "/" },
+    { key: 2, name: "About Us", link: "/about" },
+    { key: 3, name: "Services", link: "/services" },
+    { key: 4, name: "Packages", link: "/packages" },
+    { key: 5, name: "Contact Us", link: "/contact" },
+  ];
+
+  return (
+    <Container fluid px={0}> 
+      <Flex
+        direction="row"
+        align="center"
+        justify="space-between"
+        px="md" 
+        py="xs"
+        bg={'black'}
+        px={100}
+        my={20}
+        mx={100}
+        style={{
+          borderRadius: '23px', 
+          
+        }}
+      >
+        <Image src={headerImage} alt="Logo" width={193} mx={100} height={64} />
+        
+        <Flex
+         mx={100}
+          direction="row"
+          align="center"
+          gap="md"
+          
+        >
+          {navlinks.map((item) => (
+            <NavLink
+              fz={20} 
+              key={item.key}
+              label={item.name}
+              href={item.link}
+              className={classes.navLink}
+            />
+          ))}
+          <Button fz={20} h={47} w={189} className={classes.agentPortal}>Agent Portal</Button>
+          <Button fz={20} h={47} w={189}  className={classes.login}>Login</Button>
+        </Flex>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Header;
