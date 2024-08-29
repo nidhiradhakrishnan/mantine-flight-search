fd22999d51c6c7391c311579475d0b057ba0496c
Sreenidhi S R <sreenidhisr07@gmail.com>
2024-08-29 13:50:15 +0530

    feat:"faq completed and responsive"

--- src/components/faq/style.js
+++ src/components/faq/style.js
@@ -4,5 +4,5 @@ const useStyle = createStyles (()=>({
         background: 'transparent linear-gradient(180deg, #F8E4B1 0%, #F2E8CE 100%) 0% 0% no-repeat padding-box',
     },
 
-}));
+}))
 export default useStyle;
